e2a217d67f066d8f655f1c33ca7e2f914cad4208
vinaygvalsaraj <vinaygvalsaraj@gmail.com>
2018-01-31 01:26:48 -0600

    Styling of each Tile completed. No overflow from words, margins on top and bottom (padding already on sides), and bolded title.

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -9,13 +9,31 @@ const ResponsiveTile = styled.div`
   padding: 0.5em;
 `
 
+const TextBox = styled(Paragraph)`
+  max-width: unset;
+  margin-top: 0.5em;
+  margin-bottom: 0.5em;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`
+
+const TileTitle = styled.div`
+  font-weight: 1000;
+`
+
+const Channel = styled.div`
+  font-weight: 500;
+`
+
 function Tile(props) {
   return (
     <ResponsiveTile>
       <img src={props.video.snippet.thumbnails.high.url}/>
-      <div>{props.video.snippet.title}</div>
-      <div>{props.video.snippet.channelTitle}</div>
-      <div>{props.video.snippet.description}</div>
+      <TextBox>
+        <TileTitle>{props.video.snippet.title}</TileTitle>
+        <Channel>{props.video.snippet.channelTitle}</Channel>
+        <div>{props.video.snippet.description}</div>
+      </TextBox>
     </ResponsiveTile>
   )
 }
