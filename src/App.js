ca312620a9481b44e41f82d5b29f422ace23223e
vinaygvalsaraj <vinaygvalsaraj@gmail.com>
2018-01-31 00:55:56 -0600

    Added simple hnavbar and a home page. Will begin to implement grid of videos from YouTube api

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GrommetApp from 'grommet/components/App';
+import { Box } from 'grommet';
+
+import './assets/grommet.min.css';
+
+import Home from './components/Home';
+import NavBar from './components/NavBar';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <GrommetApp centered={false}>
+          <NavBar/>
+          <Box>
+            <Switch>
+              <Route exact path="/" component={Home}/>
+            </Switch>
+          </Box>
+        </GrommetApp>
+      </Router>
     );
   }
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { Title } from 'grommet';
+
+import styled from 'styled-components';
+
+const Container = styled.div`
+  position: relative;
+  width: 100%;
+  display: flex;
+  padding: 1em 5em;
+
+  border: 1px lightgrey;
+  border-style: none none solid;
+`
+
+class NavBar extends React.Component {
+  render() {
+    return (
+      <Container>
+        <Title>
+          Surf Videos
+        </Title>
+      </Container>
+    )
+  }
+}
+
+export default NavBar;
